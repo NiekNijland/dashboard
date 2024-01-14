2c01a2e971472b6f1ed720220a396353086a3b24
NiekNijland <niekjnijland@gmail.com>
2024-01-14 14:15:07 +0100

    Add logo and tailwind forms plugin.

--- resources/views/components/auth/heading-component.blade.php
+++ resources/views/components/auth/heading-component.blade.php
@@ -1,5 +1,5 @@
 <div class="sm:mx-auto sm:w-full sm:max-w-md">
-    <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+    <img class="mx-auto h-24 w-auto" src="{{ asset('logo.png') }}" alt="{{ config('app.name') }}">
     <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
         {{ $title }}
     </h2>
