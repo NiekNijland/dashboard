691e1f44af177fd28e44be60d08420a65b9f8edf
NiekNijland <niekjnijland@gmail.com>
2024-01-17 19:30:05 +0100

    Test deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf vendor
+
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
 php artisan migrate --force
@@ -12,7 +14,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-nvm use 20
+nvm use
 
 npm i
 
