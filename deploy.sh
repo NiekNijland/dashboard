13b5f98ddd821de41e9fb16007166ce2aadf1be4
NiekNijland <niekjnijland@gmail.com>
2024-01-17 19:02:32 +0100

    Test deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ php artisan optimize
 
 php artisan clear
 
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm use 20
 
